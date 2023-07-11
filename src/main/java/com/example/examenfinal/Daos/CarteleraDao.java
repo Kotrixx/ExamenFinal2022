310dca7685f16b50b2c06ce6ce136abd363bb4e0
Sebastian <a20206438@pucp.edu.pe>
2023-07-11 00:58:24 -0500

    avance0058

--- src/main/java/com/example/examenfinal/Daos/CarteleraDao.java
+++ src/main/java/com/example/examenfinal/Daos/CarteleraDao.java
@@ -1,6 +1,8 @@
 package com.example.examenfinal.Daos;
 
+import com.example.examenfinal.Beans.Cadena;
 import com.example.examenfinal.Beans.Cartelera;
+import com.example.examenfinal.Beans.Cine;
 import com.example.examenfinal.Beans.Pelicula;
 
 import java.sql.Connection;
@@ -14,14 +16,33 @@ public class CarteleraDao extends BaseDao{
     public ArrayList<Cartelera>listaFunciones(){
 
         ArrayList<Cartelera> lista=new ArrayList<>();
-        String sql="";
+        String sql="select cart.idCartelera as \"idCartelera\",cad.nombre_comercial as \"nombre comercial\", cin.nombre as \"cine\",peli.nombre as \"pelicula\",3d,cart.subtitulada,cart.doblada, cart.horario from cartelera cart\n" +
+                "inner join cine cin on cin.idcine=cart.idcine\n" +
+                "inner join cadena cad on cad.idcadena=cin.idcadena\n" +
+                "inner join pelicula peli on peli.idpelicula=cart.idpelicula\n" +
+                "order by idCartelera;";
         try(Connection connection=this.getConnection();
             Statement stmt=connection.createStatement();
             ResultSet resultSet=stmt.executeQuery(sql)){
 
             while(resultSet.next()){
-                Cartelera cartelera=new Cartelera();
 
+                Cartelera cartelera=new Cartelera();
+                cartelera.setIdCartelera(resultSet.getInt("idCartelera'"));
+                Pelicula pelicula=new Pelicula();
+                pelicula.setNombre(resultSet.getString("pelicula"));
+                Cine cine=new Cine();
+                cine.setNombre(resultSet.getString("cine"));
+                cine.getCadena().setNombreComercial(resultSet.getString("nombre comercial"));
+                cartelera.setCine(cine);
+                cartelera.setPelicula(pelicula);
+
+                cartelera.setTresD(resultSet.getInt(5));
+                cartelera.setSubtitulada(resultSet.getInt(6));
+                cartelera.setDoblada(resultSet.getInt(7));
+                cartelera.setHorario(resultSet.getString(8));
+
+                lista.add(cartelera);
 
             }
 
